8e8d0dd66bfe5acb557cd0c322f705ff548949ef
KyleDDJ <kdejesus16692@student.dmmmsu.edu.ph>
2025-08-05 11:32:21 +0800

    Building welcome screen

--- App.js
+++ App.js
@@ -1,22 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, React } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  console.log('App Executed');
-
-  return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
