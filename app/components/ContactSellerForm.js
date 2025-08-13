b675317949cd97d3c37f12f21d52fe74cd5d1199
KyleDDJ <kdejesus16692@student.dmmmsu.edu.ph>
2025-08-13 22:30:36 +0800

    building the contact form

---
+++ app/components/ContactSellerForm.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Button, Alert, Keyboard } from "react-native";
+import { Formik } from "formik";
+import AppTextInput from "../components/AppTextInput";
+import messagesApi from "../api/messages";
+import * as Notifications from "expo-notifications";
+
+function ContactSellerForm({ listing }) {
+  const handleSubmit = async ({ message }, { resetForm }) => {
+    Keyboard.dismiss();
+
+    const result = await messagesApi.send(message, listing.id);
+    if (!result.ok) {
+      console.log("Error", result);
+      return Alert.alert("Error", "Could not send the message.");
+    }
+
+    resetForm();
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Awesome!",
+        body: "Your message was sent.",
+      },
+      trigger: null,
+    });
+  };
+
+  return (
+    <Formik initialValues={{ message: "" }} onSubmit={handleSubmit}>
+      {({ handleChange, handleSubmit, values }) => (
+        <View>
+          <AppTextInput
+            placeholder="Message..."
+            onChangeText={handleChange("message")}
+            value={values.message}
+          />
+          <Button title="Contact Seller" onPress={handleSubmit} />
+        </View>
+      )}
+    </Formik>
+  );
+}
+
+export default ContactSellerForm;
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { View, ScrollView, Text } from "react-native";
+import {
+  View,
+  ScrollView,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { Image } from "react-native-expo-image-cache";
 import stylesDetails from "../styles/ListingDetailsScreen.styles";
 import AppText from "../components/AppText";
@@ -15,46 +21,54 @@ function ListingDetailsScreen({ route }) {
   return (
     <Screen style={{ backgroundColor: Colors.white }}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <Image
-          style={stylesDetails.image}
-          tint="light"
-          preview={{ uri: listing.images[0].thumbnailUrl }}
-          // source={{ uri: listing.images[0].url }}
-          uri={listing.images[0].url}
-          resizeMode="cover"
-        />
-
-        <View style={stylesAccount.container}>
-          <ListItem
-            title={user.name}
-            subTitle={`5 Listings`}
-            image={userImage}
-            showChevron={true}
+        <KeyboardAvoidingView
+          behavior="position"
+          keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 100}
+        >
+          <Image
+            style={stylesDetails.image}
+            tint="light"
+            preview={{ uri: listing.images[0].thumbnailUrl }}
+            // source={{ uri: listing.images[0].url }}
+            uri={listing.images[0].url}
+            resizeMode="cover"
           />
-        </View>
 
-        <View style={stylesDetails.details_container}>
-          <View style={stylesDetails.price_row}>
-            <AppText style={stylesDetails.title}>{listing.title}</AppText>
-            <View style={stylesDetails.price_discount}>
-              <Text style={stylesDetails.price}>${listing.price}</Text>
+          <View style={stylesAccount.container}>
+            <ListItem
+              title={user.name}
+              subTitle={`5 Listings`}
+              image={userImage}
+              showChevron={true}
+            />
+          </View>
+
+          <View style={stylesDetails.details_container}>
+            <View style={stylesDetails.price_row}>
+              <AppText style={stylesDetails.title}>{listing.title}</AppText>
+              <View style={stylesDetails.price_discount}>
+                <Text style={stylesDetails.price}>${listing.price}</Text>
+              </View>
             </View>
-          </View>
 
-          <View style={stylesDetails.tab_row}>
-            <Text style={[stylesDetails.tab_text, stylesDetails.tab_active]}>
-              Details
+            <View style={stylesDetails.tab_row}>
+              <Text style={[stylesDetails.tab_text, stylesDetails.tab_active]}>
+                Details
+              </Text>
+              <Text style={stylesDetails.tab_text}>Review</Text>
+            </View>
+
+            <Text style={stylesDetails.description}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              iaculis lacinia ligula sit amet maximus. Vestibulum ac tortor
+              magna. Ut vulputate, ipsum eu placerat aliquam, risus nisl
+              eleifend lectus, eget tempor purus diam ut elit.
             </Text>
-            <Text style={stylesDetails.tab_text}>Review</Text>
           </View>
-
-          <Text style={stylesDetails.description}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            iaculis lacinia ligula sit amet maximus. Vestibulum ac tortor magna.
-            Ut vulputate, ipsum eu placerat aliquam, risus nisl eleifend lectus,
-            eget tempor purus diam ut elit.
-          </Text>
-        </View>
+          <View style={{ marginVertical: 20 }}>
+            <ContactSellerForm listing={listing} />
+          </View>
+        </KeyboardAvoidingView>
       </ScrollView>
     </Screen>
   );
